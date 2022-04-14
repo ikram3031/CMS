da5bcd120956ffb37222f0dd33532e70a169a160
ikram3031 <mdikram.3031@gmail.com>
2022-04-14 20:04:43 +0600

    1st Part

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,43 +17,36 @@
                 </div>
 
                 <!-- Blog Categories Well -->
+
+                <?php
+                 //Getting data from categories table of database
+                $query = "SELECT * FROM categories LIMIT 6";
+                $select_categories_sidebar = mysqli_query($connection, $query);
+                
+                ?>
+
                 <div class="well">
                     <h4>Blog Categories</h4>
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-lg-12">
                             <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
+                                <?php
+                                //looping to fetch the category and print them in list
+                                while($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                
+                                $cat_title = $row['cat_title'];
+                
+                                echo "<li><a href='#'>{$cat_title}</a></li>";
+                                }
+                                ?>
                             </ul>
                         </div>
-                        <!-- /.col-lg-6 -->
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                                <li><a href="#">Category Name</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-lg-6 -->
+
                     </div>
                     <!-- /.row -->
                 </div>
 
                 <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
+                <?php include "widget.php" ?>;
 
             </div>
